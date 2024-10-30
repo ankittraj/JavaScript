cdefb33beb944328e158dab7649a69b632ef87ad
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2024-10-30 23:07:42 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,18 @@
+var firstName = "Ankit";
+// use a variable 
+console.log(firstName);
+
+// change value 
+firstName = "Nitish";
+
+console.log(firstName);
+
+
+
+let secondName = "Ankit";
+secondName = "Nitish";
+console.log(firstName);
+
+
+const pi = 3.14;
+console.log(pi);
