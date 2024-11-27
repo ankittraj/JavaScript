d98f32b753e1bab6ac636201f5333aca6db78bc4
ANKIT RAJ <110198496+ankittraj@users.noreply.github.com>
2024-11-27 23:55:34 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,21 @@
+// Create an Object
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+/*
+// Create an Object
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
+
+
+// Create an Object
+const person = {};
+
+// Add Properties
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue"   */
